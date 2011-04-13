fd7c2d1cb9c9867d3317b47b7146ca1d0f2728ff
Aaron Day <daya@crazydays.org>
2011-04-12 20:44:51 -0400

    Formatted.

--- ruby.edu/deck/suit.rb
+++ ruby.edu/deck/suit.rb
@@ -2,48 +2,48 @@ class InvalidSuitError < StandardError
 end
 
 class Suit
-	attr_accessor :value
+  attr_accessor :value
 
-	def initialize(value)
-		case value
-		when :spade
-		when :diamond
-		when :club
-		when :heart
-		else
-			raise InvalidSuitError, "Unknown suit: " + value.to_s
-		end
+  def initialize(value)
+    case value
+    when :spade
+    when :diamond
+    when :club
+    when :heart
+    else
+      raise InvalidSuitError, "Unknown suit: " + value.to_s
+    end
 
-		@value = value
-	end
+    @value = value
+  end
 
-	def isBlack?
-		@value == :spade || @value == :club
-	end
+  def isBlack?
+    @value == :spade || @value == :club
+  end
 
-	def isRed?
-		@value == :heart || @value == :diamond
-	end
+  def isRed?
+    @value == :heart || @value == :diamond
+  end
 
-	def eql?(s)
-		@value == s.value
-	end
+  def eql?(s)
+    @value == s.value
+  end
 
-	def to_s
-		case @value
-		when :spade
-			'spades'
-		when :diamond
-			'diamonds'
-		when :club
-			'clubs'
-		when :heart
-			'hearts'
-		end
-	end
+  def to_s
+    case @value
+    when :spade
+      'spades'
+    when :diamond
+      'diamonds'
+    when :club
+      'clubs'
+    when :heart
+      'hearts'
+    end
+  end
 
-	SPADE = Suit.new(:spade)
-	DIAMOND = Suit.new(:diamond)
-	CLUB = Suit.new(:club)
-	HEART = Suit.new(:heart)
+  SPADE = Suit.new(:spade)
+  DIAMOND = Suit.new(:diamond)
+  CLUB = Suit.new(:club)
+  HEART = Suit.new(:heart)
 end
--- ruby.edu/deck/tc_suit.rb
+++ ruby.edu/deck/tc_suit.rb
@@ -2,44 +2,43 @@ require 'suit.rb'
 require 'test/unit'
 
 class TestSuit < Test::Unit::TestCase
-	def test_initialize
-		suit = Suit.new(:spade)
-		assert_equal(:spade, suit.value)
+  def test_initialize
+    suit = Suit.new(:spade)
+    assert_equal(:spade, suit.value)
+    assert_raise(InvalidSuitError) { s5 = Suit.new(:wrong) }
+  end
 
-		assert_raise(InvalidSuitError) { s5 = Suit.new(:wrong) }
-	end
+  def test_isBlack?
+    s1 = Suit.new(:spade)
+    assert(s1.isBlack?)
 
-	def test_isBlack?
-		s1 = Suit.new(:spade)
-		assert(s1.isBlack?)
+    s2 = Suit.new(:club)
+    assert(s2.isBlack?)
+  end
 
-		s2 = Suit.new(:club)
-		assert(s2.isBlack?)
-	end
+  def test_isRed?
+    s1 = Suit.new(:heart)
+    assert(s1.isRed?)
 
-	def test_isRed?
-		s1 = Suit.new(:heart)
-		assert(s1.isRed?)
+    s2 = Suit.new(:diamond)
+    assert(s2.isRed?)
+  end
 
-		s2 = Suit.new(:diamond)
-		assert(s2.isRed?)
-	end
+  def test_to_s
+    s1 = Suit.new(:spade)
+    assert_equal("spades", s1.to_s)
+    s2 = Suit.new(:diamond)
+    assert_equal("diamonds", s2.to_s)
+    s3 = Suit.new(:club)
+    assert_equal("clubs", s3.to_s)
+    s4 = Suit.new(:heart)
+    assert_equal("hearts", s4.to_s)
+  end
 
-	def test_to_s
-		s1 = Suit.new(:spade)
-		assert_equal("spades", s1.to_s)
-		s2 = Suit.new(:diamond)
-		assert_equal("diamonds", s2.to_s)
-		s3 = Suit.new(:club)
-		assert_equal("clubs", s3.to_s)
-		s4 = Suit.new(:heart)
-		assert_equal("hearts", s4.to_s)
-	end
-
-	def test_class_constants
-		assert(Suit.new(:spade).eql?   Suit::SPADE)
-		assert(Suit.new(:diamond).eql? Suit::DIAMOND)
-		assert(Suit.new(:club).eql?    Suit::CLUB)
-		assert(Suit.new(:heart).eql?   Suit::HEART)
-	end
+  def test_class_constants
+    assert(Suit.new(:spade).eql?   Suit::SPADE)
+    assert(Suit.new(:diamond).eql? Suit::DIAMOND)
+    assert(Suit.new(:club).eql?    Suit::CLUB)
+    assert(Suit.new(:heart).eql?   Suit::HEART)
+  end
 end
