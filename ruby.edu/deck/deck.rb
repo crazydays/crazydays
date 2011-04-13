95b8673943931cf20c97c2b55f2a69a3dad3a860
Aaron Day <daya@crazydays.org>
2011-04-12 20:49:49 -0400

    Reformatted.

--- ruby.edu/deck/deck.rb
+++ ruby.edu/deck/deck.rb
@@ -4,29 +4,29 @@ class OutOfCards < StandardError
 end
 
 class Deck
-	attr_accessor :cards
+  attr_accessor :cards
 
-	def initialize()
-		@cards = Array.new
-		([Suit::SPADE, Suit::DIAMOND, Suit::CLUB, Suit::HEART]).each do |suit|
-			(1..13).each { |i| @cards.push(Card.new(suit, i)) }
-		end
-	end
+  def initialize()
+    @cards = Array.new
+    ([Suit::SPADE, Suit::DIAMOND, Suit::CLUB, Suit::HEART]).each do |suit|
+      (1..13).each { |i| @cards.push(Card.new(suit, i)) }
+    end
+  end
 
-	def shuffle!
-		@cards.shuffle!
-	end
+  def shuffle!
+    @cards.shuffle!
+  end
 
-	def empty?
-		@cards.empty?
-	end
+  def empty?
+    @cards.empty?
+  end
 
-	def deal
-		raise OutOfCards if self.empty?
-		@cards.pop
-	end
+  def deal
+    raise OutOfCards if self.empty?
+    @cards.pop
+  end
 
-	def eql? (deck)
-		@cards.eql? deck.cards
-	end
+  def eql? (deck)
+    @cards.eql? deck.cards
+  end
 end
--- ruby.edu/deck/tc_deck.rb
+++ ruby.edu/deck/tc_deck.rb
@@ -2,47 +2,47 @@ require 'deck.rb'
 require 'test/unit'
 
 class TestDeck < Test::Unit::TestCase
-	def test_constructor
-		deck = Deck.new
+  def test_constructor
+    deck = Deck.new
 
-		assert_equal(            1, deck.cards[ 0].value)
-		assert_equal(  Suit::SPADE, deck.cards[ 0].suit)
-		assert_equal(            1, deck.cards[13].value)
-		assert_equal(Suit::DIAMOND, deck.cards[13].suit)
-		assert_equal(            1, deck.cards[26].value)
-		assert_equal(   Suit::CLUB, deck.cards[26].suit)
-		assert_equal(            1, deck.cards[39].value)
-		assert_equal(  Suit::HEART, deck.cards[39].suit)
-	end
+    assert_equal(            1, deck.cards[ 0].value)
+    assert_equal(  Suit::SPADE, deck.cards[ 0].suit)
+    assert_equal(            1, deck.cards[13].value)
+    assert_equal(Suit::DIAMOND, deck.cards[13].suit)
+    assert_equal(            1, deck.cards[26].value)
+    assert_equal(   Suit::CLUB, deck.cards[26].suit)
+    assert_equal(            1, deck.cards[39].value)
+    assert_equal(  Suit::HEART, deck.cards[39].suit)
+  end
 
-	def test_shuffle!
-		d1 = Deck.new
-		d2 = Deck.new
-		d2.shuffle!
+  def test_shuffle!
+    d1 = Deck.new
+    d2 = Deck.new
+    d2.shuffle!
 
-		assert(! (d1.eql? d2))
-	end
+    assert(! (d1.eql? d2))
+  end
 
-	def test_empty?
-		d1 = Deck.new
-		assert(! (d1.empty?))
-		d1.cards = Array.new
-		assert(d1.empty?)
-	end
+  def test_empty?
+    d1 = Deck.new
+    assert(! (d1.empty?))
+    d1.cards = Array.new
+    assert(d1.empty?)
+  end
 
-	def test_deal
-		d1 = Deck.new
-		c1 = d1.deal
-		assert(c1.eql? Card.new(Suit::HEART, 13))
+  def test_deal
+    d1 = Deck.new
+    c1 = d1.deal
+    assert(c1.eql? Card.new(Suit::HEART, 13))
 
-		d1.cards = Array.new
-		assert_raise(OutOfCards) { d1.deal }
-	end
+    d1.cards = Array.new
+    assert_raise(OutOfCards) { d1.deal }
+  end
 
-	def test_eql?
-		d1 = Deck.new
-		d2 = Deck.new
+  def test_eql?
+    d1 = Deck.new
+    d2 = Deck.new
 
-		assert(d1.eql? d2)
-	end
+    assert(d1.eql? d2)
+  end
 end
