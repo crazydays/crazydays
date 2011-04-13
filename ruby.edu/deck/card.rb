94d2be6254ae0e8c58a57b849aa9a41c213db2d5
Aaron Day <daya@crazydays.org>
2011-04-12 20:44:16 -0400

    Formatted.

--- ruby.edu/deck/card.rb
+++ ruby.edu/deck/card.rb
@@ -4,38 +4,38 @@ class InvalidCardError < StandardError
 end
 
 class Card
-	attr_accessor :suit, :value
+  attr_accessor :suit, :value
 
-	def initialize(suit, value)
-		@suit = suit
-		raise InvalidCardError unless (1..13) === value
-		@value = value
-	end
+  def initialize(suit, value)
+    @suit = suit
+    raise InvalidCardError unless (1..13) === value
+    @value = value
+  end
 
-	def to_s
-		"#{value_to_s} of #{suit_to_s}"
-	end
+  def to_s
+    "#{value_to_s} of #{suit_to_s}"
+  end
 
-	def value_to_s
-		case @value
-		when 13
-			'king'
-		when 12
-			'queen'
-		when 11
-			'jack'
-		when 1
-			'ace'
-		else
-			@value.to_s
-		end
-	end
+  def value_to_s
+    case @value
+    when 13
+      'king'
+    when 12
+      'queen'
+    when 11
+      'jack'
+    when 1
+      'ace'
+    else
+      @value.to_s
+    end
+  end
 
-	def suit_to_s
-		@suit.to_s
-	end
+  def suit_to_s
+    @suit.to_s
+  end
 
-	def eql?(card)
-		@suit.eql?(card.suit) && @value.eql?(card.value)
-	end
+  def eql?(card)
+    @suit.eql?(card.suit) && @value.eql?(card.value)
+  end
 end
--- ruby.edu/deck/tc_card.rb
+++ ruby.edu/deck/tc_card.rb
@@ -2,56 +2,55 @@ require 'card.rb'
 require 'test/unit'
 
 class TestCard < Test::Unit::TestCase
-	def test_initialize
-		card = Card.new(Suit::SPADE, 1)
-		assert_equal(Suit::SPADE, card.suit)
-		assert_equal(1, card.value)
+  def test_initialize
+    card = Card.new(Suit::SPADE, 1)
+    assert_equal(Suit::SPADE, card.suit)
+    assert_equal(1, card.value)
 
-		assert_raise(InvalidCardError) { Card.new(Suit::HEART, -1) }
-		assert_raise(InvalidCardError) { Card.new(Suit::HEART, 0) }
-		assert_raise(InvalidCardError) { Card.new(Suit::HEART, 14) }
-	end
+    assert_raise(InvalidCardError) { Card.new(Suit::HEART, -1) }
+    assert_raise(InvalidCardError) { Card.new(Suit::HEART, 0) }
+    assert_raise(InvalidCardError) { Card.new(Suit::HEART, 14) }
+  end
 
-	def test_to_s
-		assert_equal('king of spades', Card.new(Suit::SPADE,13).to_s)
-		assert_equal( '7 of diamonds', Card.new(Suit::DIAMOND, 7).to_s)
-		assert_equal(    '6 of clubs', Card.new(Suit::CLUB, 6).to_s)
-		assert_equal( 'ace of hearts', Card.new(Suit::HEART, 1).to_s)
+  def test_to_s
+    assert_equal('king of spades', Card.new(Suit::SPADE,13).to_s)
+    assert_equal( '7 of diamonds', Card.new(Suit::DIAMOND, 7).to_s)
+    assert_equal(    '6 of clubs', Card.new(Suit::CLUB, 6).to_s)
+    assert_equal( 'ace of hearts', Card.new(Suit::HEART, 1).to_s)
+  end
 
-	end
+  def test_value_to_s
+    assert_equal( 'king', Card.new(Suit::SPADE, 13).value_to_s)
+    assert_equal('queen', Card.new(Suit::SPADE, 12).value_to_s)
+    assert_equal( 'jack', Card.new(Suit::SPADE, 11).value_to_s)
+    assert_equal(   '10', Card.new(Suit::SPADE, 10).value_to_s)
+    assert_equal(    '9', Card.new(Suit::SPADE,  9).value_to_s)
+    assert_equal(    '8', Card.new(Suit::SPADE,  8).value_to_s)
+    assert_equal(    '7', Card.new(Suit::SPADE,  7).value_to_s)
+    assert_equal(    '6', Card.new(Suit::SPADE,  6).value_to_s)
+    assert_equal(    '5', Card.new(Suit::SPADE,  5).value_to_s)
+    assert_equal(    '4', Card.new(Suit::SPADE,  4).value_to_s)
+    assert_equal(    '3', Card.new(Suit::SPADE,  3).value_to_s)
+    assert_equal(    '2', Card.new(Suit::SPADE,  2).value_to_s)
+    assert_equal(  'ace', Card.new(Suit::SPADE,  1).value_to_s)
+  end
 
-	def test_value_to_s
-		assert_equal( 'king', Card.new(Suit::SPADE, 13).value_to_s)
-		assert_equal('queen', Card.new(Suit::SPADE, 12).value_to_s)
-		assert_equal( 'jack', Card.new(Suit::SPADE, 11).value_to_s)
-		assert_equal(   '10', Card.new(Suit::SPADE, 10).value_to_s)
-		assert_equal(    '9', Card.new(Suit::SPADE,  9).value_to_s)
-		assert_equal(    '8', Card.new(Suit::SPADE,  8).value_to_s)
-		assert_equal(    '7', Card.new(Suit::SPADE,  7).value_to_s)
-		assert_equal(    '6', Card.new(Suit::SPADE,  6).value_to_s)
-		assert_equal(    '5', Card.new(Suit::SPADE,  5).value_to_s)
-		assert_equal(    '4', Card.new(Suit::SPADE,  4).value_to_s)
-		assert_equal(    '3', Card.new(Suit::SPADE,  3).value_to_s)
-		assert_equal(    '2', Card.new(Suit::SPADE,  2).value_to_s)
-		assert_equal(  'ace', Card.new(Suit::SPADE,  1).value_to_s)
-	end
+  def test_suit_to_s
+    assert_equal(  'spades', Card.new(Suit::SPADE, 1).suit_to_s)
+    assert_equal('diamonds', Card.new(Suit::DIAMOND, 1).suit_to_s)
+    assert_equal(   'clubs', Card.new(Suit::CLUB, 1).suit_to_s)
+    assert_equal(  'hearts', Card.new(Suit::HEART, 1).suit_to_s)
+  end
 
-	def test_suit_to_s
-		assert_equal(  'spades', Card.new(Suit::SPADE, 1).suit_to_s)
-		assert_equal('diamonds', Card.new(Suit::DIAMOND, 1).suit_to_s)
-		assert_equal(   'clubs', Card.new(Suit::CLUB, 1).suit_to_s)
-		assert_equal(  'hearts', Card.new(Suit::HEART, 1).suit_to_s)
-	end
+  def test_eql?
+    c1 = Card.new(Suit::SPADE, 1)
+    c2 = Card.new(Suit::SPADE, 1)
+    c3 = Card.new(Suit::HEART, 1)
+    c4 = Card.new(Suit::SPADE, 2)
 
-	def test_eql?
-		c1 = Card.new(Suit::SPADE, 1)
-		c2 = Card.new(Suit::SPADE, 1)
-		c3 = Card.new(Suit::HEART, 1)
-		c4 = Card.new(Suit::SPADE, 2)
-
-		assert(c1.eql? c1)
-		assert(c1.eql? c2)
-		assert(! (c1.eql? c3))
-		assert(! (c1.eql? c4))
-	end
+    assert(c1.eql? c1)
+    assert(c1.eql? c2)
+    assert(! (c1.eql? c3))
+    assert(! (c1.eql? c4))
+  end
 end
